548c106db52831f44e9c14676509604dab771690
nicholas blackburn <nickblackburn02@gmail.com>
2019-11-04 08:44:02 -0500

    added ip address pinger

--- src/main/java/com/nicholasblackburn/status/Network/Status.java
+++ src/main/java/com/nicholasblackburn/status/Network/Status.java
@@ -1,10 +1,27 @@
 package com.nicholasblackburn.status.Network;
 
-public class Status{
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Status {
 
     // runs a ping to provided ip for 100 ms to calculate the tps
-public static void status(){
+public static void status() throws UnknownHostException{
 
+    String addr = "10.57.40.2"; // ip address of the roborio on the robot
+     
+    try{
+        InetAddress address = InetAddress.getByName(addr);
+        boolean reachable = address.isReachable(10000);
+
+        System.out.println("Is host reachable? " + reachable);
+
+    } 
+    catch (Exception e){
+        
+        e.printStackTrace();
+    }
 
     }
 
